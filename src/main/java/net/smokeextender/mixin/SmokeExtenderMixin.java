5ceb196f6f55bb87f47d2b4ed03f15ba1fee44d5
AfterRebelion <559819+AfterRebelion@users.noreply.github.com>
2019-04-04 01:27:03 +0200

    Update mappings
    Now all classes have yarn names.

--- src/main/java/net/smokeextender/mixin/SmokeExtenderMixin.java
+++ src/main/java/net/smokeextender/mixin/SmokeExtenderMixin.java
@@ -3,7 +3,7 @@ package net.smokeextender.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-import net.minecraft.class_3940;
+import net.minecraft.client.particle.SpriteBillboardParticle;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,14 +11,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(net.minecraft.class_3937.class)
-public abstract class SmokeExtenderMixin extends class_3940 {
+@Mixin(net.minecraft.client.particle.CampfireSmokeParticle.class)
+public abstract class SmokeExtenderMixin extends SpriteBillboardParticle {
 	public SmokeExtenderMixin(World world_1, double double_1, double double_2, double double_3, double double_4, double double_5, double double_6) {
 		super(world_1, double_1, double_2, double_3, double_4, double_5, double_6);
 	}
 
 	@Inject(at = @At("RETURN"), method = "<init>(Lnet/minecraft/world/World;DDDDDDZ)V")
-	private void class_3937(World world_1, double double_1, double double_2, double double_3, double double_4, double double_5, double double_6, boolean boolean_1, CallbackInfo info) {
+	private void CampfireSmokeParticle (World world_1, double double_1, double double_2, double double_3, double double_4, double double_5, double double_6, boolean boolean_1, CallbackInfo info) {
 		if (boolean_1) {
 			// System.out.println("Injecting on the smoke constructor!");
 			this.maxAge += 560;
